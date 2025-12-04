c4315b3ab2837cdb9f6b65b3780828230d9a36ac
VincentWilson <v1wilson@student.bridgew.edu>
2025-12-04 15:37:16 -0500

    I added the texture registry

--- main.py
+++ main.py
@@ -4,10 +4,28 @@ import dearpygui.dearpygui as graphics
 
 
 graphics.create_context()
+#This is some of the player variables
 player_y = 100
 player_x = 100
 player_speed = 5
+#This code loads the image
 player_w, player_h, channels, player_raw_data = graphics.load_image("PersonSprite.png")
+#This code sets up the movement for the character
+def move_player(sender, app_data):
+    global player_x, player_y, player_speed, player_w, player_h
+    key = app_data
+    if key == graphics.mvKey_Left:
+        player_x -= player_speed
+    elif key == graphics.mvKey_Right:
+        player_x += player_speed
+    elif key == graphics.mvKey_Up:
+        player_y -= player_speed
+    elif key == graphics.mvKey_Down:
+        player_y += player_speed
+    with graphics.mutex():
+        graphics.configure_item("player_update", pmin=(player_x, player_y), pmax=(player_x+player_w, player_y+player_h))
+with graphics.texture_registry():
+    graphics.add_static_texture(player_w, player_h, player_raw_data, tag="PersonSprite")
 graphics.create_viewport(title="Project 7", width=1480, height=1200)
 with graphics.window(label="Project 7", width=1600, height=1200):
     with graphics.drawlist(width=1500, height=800):
