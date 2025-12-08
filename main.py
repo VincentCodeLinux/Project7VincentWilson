687d6e15f41aca75a9dab9f0c2d18854e1702de4
VincentWilson <v1wilson@student.bridgew.edu>
2025-12-08 10:06:27 -0500

    I created the function for the third set of cars

--- main.py
+++ main.py
@@ -262,9 +262,25 @@ def move_bear():
 
 
     pass
+def move_cavalier():
+    global car_x, car_y, shrink_car_w, shrink_car_h, car1_x, car1_y, shrink_car1_w, shrink_car1_h, car2_x, car2_y, shrink_car2_w, shrink_car2_h
+    firebird_x -= 2
+    if firebird_x < 0:
+        firebird_x = 1500
+    firebird1_x -= 2
+    if firebird1_x < 0:
+        firebird1_x = 1500
+    firebird2_x -= 2
+    if firebird2_x < 0:
+        firebird2_x = 1500
+    graphics.configure_item("firebird_update", pmin=(firebird_x, firebird_y),
+                            pmax=(firebird_x + shrink_firebird_w, firebird_y + shrink_firebird_h))
+    graphics.configure_item("firebird1_update", pmin=(firebird1_x, firebird1_y),
+                            pmax=(firebird1_x + shrink_firebird1_w, firebird1_y + shrink_firebird1_h))
+    graphics.configure_item("firebird2_update", pmin=(firebird2_x, firebird2_y),
+                            pmax=(firebird2_x + shrink_firebird2_w, firebird2_y + shrink_firebird2_h))
 
-
-
+    pass
 
 
 def collisioncar_check():
